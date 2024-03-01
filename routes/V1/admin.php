e956c0ef158a6c675618c9da863d6c61364e66e1
moaoa <moaadbn3@gmail.com>
2024-03-01 14:49:12 +0200

    make the group_id nullable in the student table

--- routes/V1/admin.php
+++ routes/V1/admin.php
@@ -13,9 +13,9 @@ Route::post('/login', [AdminController::class, 'login']);
 Route::post('/register', [AdminController::class, 'store']);
 
 
-   // TODO: TO BE AUTHENTICATED AND AUTHORIZED ONLY FOR ADMINS
-   Route::post('/teacher-to-subject-attachment',AssignSubjectToTeacher::class);
-   
+// TODO: TO BE AUTHENTICATED AND AUTHORIZED ONLY FOR ADMINS
+Route::post('/teacher-to-subject-attachment', AssignSubjectToTeacher::class);
+
 // TODO: TO BE AUTHENTICATED AND AUTHORIZED ONLY FOR ADMINS
 Route::post('/teacher-to-subject-attachment', [AdminController::class, 'assignSubjectsToTeacher']);
 Route::post('/student-assignment', [AdminController::class, 'assignStudentToSemester']);
@@ -23,9 +23,9 @@ Route::post('/student-assignment', [AdminController::class, 'assignStudentToSeme
 Route::post('/teachers', [AdminController::class, 'addTeacher']);
 Route::post('/students', [AdminController::class, 'addStudent']);
 Route::post('/lectures', [AdminController::class, 'addLecture']);
-Route::post('/teacher-to-subject-attachment',[AdminController::class, 'assignSubjectToTeacher']);
-Route::post('/student-assignment',[AdminController::class, 'assignStudentToSemester']);
+Route::post('/teacher-to-subject-attachment', [AdminController::class, 'assignSubjectToTeacher']);
+Route::post('/student-assignment', [AdminController::class, 'assignStudentToSemester']);
 
 
-Route::middleware('auth:sanctum')->get('/absence/{id}',[HybridController::class,'absenceRatio']);
-Route::middleware('auth:sanctum')->get('/grades/{id}',[HybridController::class,'studentGrades']);
+Route::middleware('auth:sanctum')->get('/absence/{id}', [HybridController::class, 'absenceRatio']);
+Route::middleware('auth:sanctum')->get('/grades/{id}', [HybridController::class, 'studentGrades']);
