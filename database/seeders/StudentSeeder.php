865337abe568a84412f9f80fbe2f1d1b3731b57d
moaoa <moaadbn3@gmail.com>
2024-03-06 22:11:06 +0200

    fix student and teacher seeders

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -19,65 +19,66 @@ class StudentSeeder extends Seeder
         //
         $names = [
             'محمد',
-            'أحمد',
-            'علي',
-            'عبدالله',
-            'يوسف',
-            'طارق',
-            'أمير',
-            'خالد',
-            'مصطفى',
-            'ياسر',
-            'محمود',
-            'حسام',
-            'عمر',
-            'حسين',
-            'ماجد',
-            'جمال',
-            'سعيد',
-            'رامي',
-            'زياد',
-            'ناصر',
-            'فاطمة',
-            'مريم',
-            'سارة',
-            'ليلى',
-            'نور',
-            'ريما',
-            'مروة',
-            'سلمى',
-            'لمى',
-            'رنا',
-            'جميلة',
-            'آمنة',
-            'داليا',
-            'هالة',
-            'رحمة',
-            'زينب',
-            'لينا',
-            'ميساء',
-            'روان',
-            'سمية'
+            // 'أحمد',
+            // 'علي',
+            // 'عبدالله',
+            // 'يوسف',
+            // 'طارق',
+            // 'أمير',
+            // 'خالد',
+            // 'مصطفى',
+            // 'ياسر',
+            // 'محمود',
+            // 'حسام',
+            // 'عمر',
+            // 'حسين',
+            // 'ماجد',
+            // 'جمال',
+            // 'سعيد',
+            // 'رامي',
+            // 'زياد',
+            // 'ناصر',
+            // 'فاطمة',
+            // 'مريم',
+            // 'سارة',
+            // 'ليلى',
+            // 'نور',
+            // 'ريما',
+            // 'مروة',
+            // 'سلمى',
+            // 'لمى',
+            // 'رنا',
+            // 'جميلة',
+            // 'آمنة',
+            // 'داليا',
+            // 'هالة',
+            // 'رحمة',
+            // 'زينب',
+            // 'لينا',
+            // 'ميساء',
+            // 'روان',
+            // 'سمية'
         ];
 
+        $counter = 1;
+
+        Student::create([
+            'name' => 'م.رحيم ديهوم',
+            'ref_number' => '1111',
+            'email'=> 'student' . $counter . '@email.com',
+            'phone_number'=> '092' . $counter,
+            'password' =>  Hash::make('password')
+        ]);
 
         foreach($names as $name){
-            $faker = Faker::create();
-
-
-            Student::create([
-                'name' => 'م.رحيم ديهوم',
-                'ref_number' => '1111',
-                'email'=> $faker->email(),
-                'phone_number'=> $faker->phoneNumber(),
-                'password' =>  Hash::make('password')
-            ]);
+            // $faker = Faker::create();
+            $counter++;
 
             Student::create([
                 'name' => $name,
                 'ref_number' => str_pad(random_int(0, 9999), 4, '0', STR_PAD_LEFT),
-                'email'=> $faker->email(),
-                'phone_number'=> $faker->phoneNumber(),
+                'email'=> 'student' . $counter . '@email.com',
+                'phone_number'=> '092' . $counter,
                 'password' =>  Hash::make('password'),
                 'group_id'=> random_int(1,15),
             ]);
