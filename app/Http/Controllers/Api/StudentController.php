c60ff4484a2de747104e51813e0c35f7d8ea2013
moaoa <moaadbn3@gmail.com>
2024-03-07 02:27:48 +0200

    status code for the lecture schedules endpoint

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -98,7 +98,7 @@ class StudentController extends Controller
         
         if ($lectureRecords->isEmpty()) {
             # code...
-            return 'لاتوجد محاضرات لهذا الطالب';
+            return response()->json(['message' => 'لاتوجد محاضرات لهذا الطالب'], 400);
         }
         return $lectureRecords;
     }
